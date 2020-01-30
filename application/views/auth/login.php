931e6304ad51b3a72ff41a9907afc13c71da3a1f
oxygenfox <andrysetyoso@gmail.com>
2020-01-30 15:05:53 +0700

    first commit

---
+++ application/views/auth/login.php
@@ -0,0 +1,68 @@
+   <div class="container">
+
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+
+            <div class="col-lg-7">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-lg">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">Login Page</h1>
+                                    </div>
+                                    <?= $this->session->flashdata('message'); ?>
+                                    <form class="user" action="<?= base_url('auth'); ?>" method="post">
+                                        <div class="form-group">
+                                            <input type="text" class="form-control form-control-user" id="email" placeholder="Enter Email Address or Username..." name="username" value="<?= set_value('username'); ?>">
+                                            <?= form_error(
+                                                'username',
+                                                '<small class="text-danger pl-3">',
+                                                '</small>'
+                                            ); ?>
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" class="form-control form-control-user" id="password" name="password" placeholder="Password">
+                                            <?= form_error(
+                                                'password',
+                                                '<small class="text-danger pl-3">',
+                                                '</small>'
+                                            ); ?>
+                                        </div>
+
+                                        <button type="submit" class="btn btn-primary btn-user btn-block">
+                                            Login
+                                        </button>
+
+                                    </form>
+                                    <hr>
+                                    <div class="text-center">
+                                        <a class="small" href="<?= base_url('auth/forgotpass'); ?>">Forgot Password?</a>
+                                    </div>
+                                    <div class="text-center">
+                                        <a class="small" href="<?= base_url('auth/registration'); ?>">Create an Account!</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+    
+  <!-- Bootstrap core JavaScript-->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="js/sb-admin-2.min.js"></script>
---
+++ application/views/menu/edit-menu.php
@@ -0,0 +1,33 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+
+    <div class="row">
+        <div class="col-lg-8">
+            <?= $this->session->flashdata('message'); ?>
+            <?= form_open('menu/editmenu/' . $menu['id']); ?>
+            <div class="form-group row">
+                <label for="email" class="col-sm-2 col-form-label">Menu name</label>
+                <div class="col-sm-10">
+                    <input type="text" name="menu" id="menu" class="form-control" value="<?= $menu['menu']; ?>">
+                    <?= form_error('menu', '<small class="text-danger pl-3">', '</small>') ?>
+                </div>
+            </div>
+            <div class="form-group row justify-content-end">
+                <div class="col-sm-10">
+                    <button type="submit" class="btn btn-primary">Edit</button>
+                    <a class="btn btn-primary" href="<?= base_url('menu/deletemenu/') . $menu['id']; ?>">Delete</a>
+                </div>
+            </div>
+
+            </form>
+
+        </div>
+    </div>
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
